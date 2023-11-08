6a9de42a0ea0ca96541a8c25ab66b0d0e69649df
Dimas Damar Buana <dimasluxmundi@gmail.com>
2023-11-08 21:23:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity5/data/OrderUIState.kt
@@ -0,0 +1,8 @@
+package com.example.activity5.data
+
+data class OrderUIState(
+    val jumlah: Int = 0,
+    val rasa: String = "",
+    val harga: String = "",
+
+)
---
+++ app/src/main/java/com/example/activity5/data/SumberData.kt
@@ -0,0 +1,14 @@
+package com.example.activity5.data
+
+import com.example.activity5.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.lemon,
+        R.string.lychee,
+        R.string.milk,
+        R.string.vanilla,
+
+    )
+}
