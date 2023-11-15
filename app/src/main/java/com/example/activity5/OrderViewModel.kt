8c455042227099a483216967b19de58d78977020
Dimas Damar Buana <dimasluxmundi@gmail.com>
2023-11-15 17:39:40 +0700

    add setNama function

--- app/src/main/java/com/example/activity5/OrderViewModel.kt
+++ app/src/main/java/com/example/activity5/OrderViewModel.kt
@@ -18,6 +18,14 @@ class OrderViewModel : ViewModel() {
     val stateUI: StateFlow<OrderUIState> = _stateUI.asStateFlow()
     val nameST : StateFlow<FormState> = _nameSTATE.asStateFlow()
 
+    fun setNama(list: MutableList<String>){
+        _nameSTATE.update { stateSaatIni -> stateSaatIni.copy(
+            nama = list[0],
+            alamat = list[1],
+            phone = list[2]
+        ) }
+    }
+
     fun setJumlah(jmlEsJumbo: Int) {
         _stateUI.update { stateSaatIni ->
             stateSaatIni.copy(
