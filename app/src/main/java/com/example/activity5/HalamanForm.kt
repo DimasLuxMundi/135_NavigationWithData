22ccc565194488cfd9f5422edf150073cd885ad0
Dimas Damar Buana <dimasluxmundi@gmail.com>
2023-11-15 16:43:45 +0700

    create form display

---
+++ app/src/main/java/com/example/activity5/HalamanForm.kt
@@ -0,0 +1,91 @@
+package com.example.activity5
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanForm(){
+
+    var namaText by remember {
+        mutableStateOf("")
+    }
+    var alamatText by remember {
+        mutableStateOf("")
+    }
+    var phoneText by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Masukan data diri", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = namaText,
+                shape = MaterialTheme.shapes.large,
+                label = { Text(text = stringResource(id = R.string.nameUser)) },
+                onValueChange = {namaText = it}
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = alamatText,
+                shape = MaterialTheme.shapes.large,
+                label = { Text(text = stringResource(id = R.string.addressUser)) },
+                onValueChange = {alamatText = it}
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = phoneText,
+                shape = MaterialTheme.shapes.large,
+                label = { Text(text = stringResource(id = R.string.phoneUser)) },
+                onValueChange = {phoneText = it}
+            )
+
+
+            Spacer(modifier = Modifier.padding(10.dp))
+        }
+        Row (modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ){
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.submit_button))
+            }
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.back_button))
+            }
+        }
+    }
+}
